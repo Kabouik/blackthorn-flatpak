fc8e85930850b9722beddaed0bcddf407ed2c994
M <matf@disr.it>
2026-08-20 23:35:11 +0200

    flatpak: expose host-os and prepend host bin dirs to PATH for Wine

--- flatpak/blackthorn-launcher.sh
+++ flatpak/blackthorn-launcher.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+# Make host system binaries (wine, etc.) visible inside the sandbox
+export PATH="/run/host/usr/local/bin:/run/host/usr/bin:$PATH"
 exec zypak-wrapper /app/lib/launcher/blackthorn-launcher "$@"
