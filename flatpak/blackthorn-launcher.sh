eff481d4f1898442888f68acac5a7cde1a2bee9c
M <matf@disr.it>
2026-08-21 10:50:18 +0200

    flatpak: allow /tmp access and preserve host locale in wrapper

--- flatpak/blackthorn-launcher.sh
+++ flatpak/blackthorn-launcher.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 export PATH="/app/wine/bin:$PATH"
+export LANG="${LANG:-C.UTF-8}"
+export LC_CTYPE="${LC_CTYPE:-${LANG}}"
 exec zypak-wrapper /app/lib/launcher/blackthorn-launcher "$@"
